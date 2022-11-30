e75ca65d58230f9e0036fc396896bea0469d368c
Ori Bendet <ori.bendet@checkmarx.com>
2022-11-29 16:34:11 -0800

    Update Login.java

--- src/Login.java
+++ src/Login.java
@@ -13,7 +13,7 @@ import java.sql.ResultSet;
 
 import javax.servlet.http.HttpServletRequest;
 
-// Class Declaration
+// Class Declarati
 class Login
 {
     public static void main(String[] args)   
